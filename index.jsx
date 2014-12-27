3b405098b12ee823d7b9a3260f998366ba473275
Cheng Lou <chenglou92@gmail.com>
2014-12-27 04:12:01 -0500

    Working prototype with stream

--- index.jsx
+++ index.jsx
@@ -1,13 +1,33 @@
 var React = require('react');
 var _ = require('lodash');
 
+var easingTypes = require('./easingTypes');
+
 var fuck = 0;
+
+function toRange(ms) {
+  return _.range(0, ms * 60 / 1000);
+}
+
+function toMs(frame) {
+  return frame * 1000 / 60;
+}
+
+function map2(arr1, arr2, f) {
+  var res = [];
+  for (var i = 0; i < Math.min(arr1.length, arr2.length); i++) {
+    var item1 = i < arr1.length ? arr1[i] : null;
+    var item2 = i < arr2.length ? arr2[i] : null;
+    res.push(f(item1, item2));
+  }
+
+  return res;
+}
 
 function playStreamFrame() {
   if (this.stream.length === 0) {
     return;
   }
-
 
   var state = this.stream.shift();
   this.setState(state);
@@ -20,15 +40,16 @@ function setStateStreamFunc(stream) {
   this.setState(state);
 }
 
-function toRange(ms) {
-  return _.range(0, ms * 60 / 1000);
-}
-
 var setStateStream = {
   setStateStream: setStateStreamFunc,
   playStreamFrame: playStreamFrame,
+
   componentWillMount: function() {
-    this.stream = [];
+    this.stream = (this.getInitialStateStream && this.getInitialStateStream()) || [];
+  },
+
+  getInitialState: function() {
+    return (this.getInitialStateStream && this.getInitialStateStream()[0]) || {};
   },
 
   componentDidMount: function() {
@@ -42,34 +63,75 @@ var setStateStream = {
   },
 };
 
-var Comp = React.createClass({
-  mixins: [setStateStream],
-  getInitialState: function() {
-    return {
-      left: 70
-    };
-  },
-
-  componentDidMount: function() {
-    var s = toRange(2000).map(function(n) {
-      return {
-        left: this.state.left + n,
-      };
-    }, this);
-    this.setStateStream(s);
-  },
-
+var Block = React.createClass({
   render: function() {
     var s = {
       border: '1px solid gray',
       width: 60,
       height: 50,
-      position: 'absolute',
-      left: this.state.left,
+      position: 'relative',
     };
+
+    var style = _.merge(s, this.props.style);
+
+    return <div {...this.props} style={style}>asd</div>;
+  }
+});
+
+var Comp = React.createClass({
+  mixins: [setStateStream],
+  getInitialStateStream: function() {
+    return [{
+      left: [50, 80, 100],
+    }];
+  },
+
+  handleClick: function(num) {
+    // this is needed for laziness. ref to stream[0] in lazy map is resolves to
+    // the wrong stream head
+    var initState = this.state;
+    var r = toRange(1000);
+    var newStream = map2(this.stream, r, function(state, n) {
+      var newState = _.cloneDeep(state);
+      newState.left[num] = easingTypes.easeOutBounce(
+        toMs(n),
+        initState.left[num],
+        initState.left[num] + 200,
+        1000
+      );
+
+      return newState;
+    });
+
+    var finalState = newStream[newStream.length - 1] || initState;
+    var moreStream = _.drop(r, newStream.length).map(function(n) {
+      var state = _.cloneDeep(finalState);
+      state.left[num] = easingTypes.easeOutBounce(
+        toMs(n),
+        initState.left[num],
+        initState.left[num] + 200,
+        1000
+      );
+
+      return state;
+    });
+
+    this.setStateStream(newStream.concat(moreStream));
+  },
+
+  render: function() {
+    // TODO: don't let render access the whole stream for now. render stays a
+    // snapshot ignorant of state history
+    var state = this.state;
+    var s1 = {left: state.left[0]};
+    var s2 = {left: state.left[1]};
+
     return (
-      <div style={s}>
-        asd
+      <div>
+        <button onClick={this.handleClick.bind(null, 0)}>click 0</button>
+        <button onClick={this.handleClick.bind(null, 1)}>click 1</button>
+        <Block style={s1}></Block>
+        <Block style={s2}></Block>
       </div>
     );
   }
